dcd6d04f6fab87cab768648add1c1bdbd78e3cce
Dharansh Neema <dharanshneema@gmail.com>
2022-12-31 12:26:28 +0530

    My first production app

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = 3000 || process.env.PORT;
+
+const format = require("date-format");
+
+app.get("/", (req, res) => {
+  res.status(200).send("Hello World!!");
+});
+app.get("/api/v1/instagram", (req, res) => {
+  const instaSocial = {
+    username: "dharanshneema",
+    followers: "900",
+    following: "1000",
+    date: format.asString("dd/MM/yy hh:mm", new Date()),
+  };
+  res.status(200).send(instaSocial);
+});
+
+app.get("/api/v1/:anyurl", (req, res) => {
+  const toReturn = req.params.anyurl;
+  res.send(toReturn);
+});
+app.listen(PORT, () => {
+  console.log(`On Port ${PORT}`);
+});
